732a3ac61e069c34245f8e88fa985bfd7d968db9
Mike Salway <38225053+msalway@users.noreply.github.com>
2020-09-13 22:34:46 +0000

    Switch to string parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,32 +7,32 @@
 #   Sets whether the ius repo is enabled by default
 #
 class ius (
-  Integer[0,1] $ius_enabled = 1,
+  Enum['0','1'] $ius_enabled = '1',
 ) {
   yumrepo { 'ius':
     descr         => 'IUS for Enterprise Linux 7 - $basearch',
     baseurl       => 'https://repo.ius.io/7/$basearch/',
     enabled       => $ius_enabled,
-    repo_gpgcheck => 0,
-    gpgcheck      => 1,
+    repo_gpgcheck => '0',
+    gpgcheck      => '1',
     gpgkey        => 'https://repo.ius.io/RPM-GPG-KEY-IUS-7',
   }
 
   yumrepo { 'ius-debuginfo':
     descr         => 'IUS for Enterprise Linux 7 - $basearch - Debug',
     baseurl       => 'https://repo.ius.io/7/$basearch/debug/',
-    enabled       => 0,
-    repo_gpgcheck => 0,
-    gpgcheck      => 1,
+    enabled       => '0',
+    repo_gpgcheck => '0',
+    gpgcheck      => '1',
     gpgkey        => 'https://repo.ius.io/RPM-GPG-KEY-IUS-7',
   }
 
   yumrepo { 'ius-source':
     descr         => 'IUS for Enterprise Linux 7 - Source',
     baseurl       => 'https://repo.ius.io/7/src/',
-    enabled       => 0,
-    repo_gpgcheck => 0,
-    gpgcheck      => 1,
+    enabled       => '0',
+    repo_gpgcheck => '0',
+    gpgcheck      => '1',
     gpgkey        => 'https://repo.ius.io/RPM-GPG-KEY-IUS-7',
   }
 
